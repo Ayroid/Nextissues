7e770181499a9ef44a1a84022dde6392bad3ea63
ayroid <ayushsk0000@gmail.com>
2024-06-02 16:36:23 +0530

    feat: 7. Integrate RadixUI
    Integrated Radix UI Component Library and created a button on Issues Page for testing

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>Issues Page</div>;
+  return (
+    <div>
+      <h1>Issues</h1>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
