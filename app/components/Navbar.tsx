c43f8247285267d03b23cbb678508f38578ef9a1
ayroid <ayushsk0000@gmail.com>
2024-06-04 20:15:45 +0530

    feat: add Login and Logout Functionality using SessionProvider

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,8 @@ import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoBug } from "react-icons/io5";
+import { Box } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
   const links = [
@@ -18,6 +20,7 @@ const Navbar = () => {
   ];
 
   const pathname = usePathname();
+  const { status, data: session } = useSession();
 
   return (
     <div className="flex px-5 h-16 border-b mb-5 space-x-5 items-center">
@@ -37,6 +40,19 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <Box>
+        <ul className="flex space-x-5">
+          {status === "authenticated" ? (
+            <li className="transition-colors hover:text-zinc-800">
+              <Link href="/api/auth/signout">Sign Out</Link>
+            </li>
+          ) : (
+            <li className="transition-colors hover:text-zinc-800">
+              <Link href="/api/auth/signin">Sign In</Link>
+            </li>
+          )}
+        </ul>
+      </Box>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import "./globals.css";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,12 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="violet" radius="small">
-          <Navbar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="violet" radius="small">
+            <Navbar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
