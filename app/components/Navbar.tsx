c22cf587804e27428e8c385670f3e1a6e91ea6b6
ayroid <ayushsk0000@gmail.com>
2024-06-04 22:59:45 +0530

    refactor: moved NavLinks and Auth Status to Separate Components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,6 +8,69 @@ import { usePathname } from "next/navigation";
 import { IoBug } from "react-icons/io5";
 
 const Navbar = () => {
+  return (
+    <div className="flex px-5 h-16 border-b mb-5 items-center">
+      <Flex justify="between" className="w-full">
+        <Flex align="center" gap="5">
+          <Link href={"/"}>
+            <IoBug />
+          </Link>
+          <NavLinks />
+        </Flex>
+        <AuthStatus />
+      </Flex>
+    </div>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  return (
+    <Box>
+      <ul className="flex space-x-5">
+        {status === "authenticated" ? (
+          <li className="transition-colors hover:text-zinc-800">
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar
+                  src={session.user?.image!}
+                  fallback="?"
+                  size="2"
+                  radius="full"
+                  className="cursor-pointer"
+                />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content className="min-w-56">
+                <DropdownMenu.Label className="gap-2 mt-2 mb-4">
+                  <Avatar
+                    src={session.user?.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                  <Text className="font-bold text-zinc-800">
+                    {session.user?.name}
+                  </Text>
+                </DropdownMenu.Label>
+                <Link href="/api/auth/signout">
+                  <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
+                </Link>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          </li>
+        ) : (
+          <li className="nav-link">
+            <Link href="/api/auth/signin">Sign In</Link>
+          </li>
+        )}
+      </ul>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
   const links = [
     {
       name: "Dashboard",
@@ -20,75 +83,20 @@ const Navbar = () => {
   ];
 
   const pathname = usePathname();
-  const { status, data: session } = useSession();
 
   return (
-    <div className="flex px-5 h-16 border-b mb-5 items-center">
-      <Flex justify="between" className="w-full">
-        <Flex align="center" gap="5">
-          <Link href={"/"}>
-            <IoBug />
-          </Link>
-          <ul className="flex space-x-5">
-            {links.map((link) => (
-              <li
-                key={link.href}
-                className={classnames(
-                  "transition-colors",
-                  "hover:text-zinc-800",
-                  {
-                    "text-zinc-800": pathname === link.href,
-                    "text-zinc-500": pathname !== link.href,
-                  }
-                )}
-              >
-                <Link href={link.href}>{link.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </Flex>
-        <Box>
-          <ul className="flex space-x-5">
-            {status === "authenticated" ? (
-              <li className="transition-colors hover:text-zinc-800">
-                <DropdownMenu.Root>
-                  <DropdownMenu.Trigger>
-                    <Avatar
-                      src={session.user?.image!}
-                      fallback="?"
-                      size="2"
-                      radius="full"
-                      className="cursor-pointer"
-                    />
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content>
-                    <DropdownMenu.Label className="gap-2 mt-2 mb-4">
-                      <Avatar
-                        src={session.user?.image!}
-                        fallback="?"
-                        size="2"
-                        radius="full"
-                        className="cursor-pointer"
-                      />
-                      <Text className="font-bold text-zinc-800">
-                        {session.user?.name}
-                      </Text>
-                    </DropdownMenu.Label>
-                    <Link href="/api/auth/signout">
-                      <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
-                    </Link>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Root>
-              </li>
-            ) : (
-              <li className="transition-colors hover:text-zinc-800">
-                <Link href="/api/auth/signin">Sign In</Link>
-              </li>
-            )}
-          </ul>
-        </Box>
-      </Flex>
-    </div>
+    <ul className="flex space-x-5">
+      {links.map((link) => (
+        <li
+          key={link.href}
+          className={classnames("nav-link", {
+            "!text-zinc-800": pathname === link.href,
+          })}
+        >
+          <Link href={link.href}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
