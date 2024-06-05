7790944f36a8febe61dd2ff0515306596f5e9166
ayroid <ayushsk0000@gmail.com>
2024-06-06 01:46:11 +0530

    refactor: removed extra imports

--- app/components/LatestIssues.tsx
+++ app/components/LatestIssues.tsx
@@ -2,7 +2,6 @@ import prisma from "@/prisma/client";
 import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import IssueStatusBadge from "./IssueStatusBadge";
-import Head from "next/head";
 
 const LatestIssues = async () => {
   const issues = await prisma.issue.findMany({
@@ -16,7 +15,9 @@ const LatestIssues = async () => {
   });
   return (
     <Card>
-      <Heading size="6" mb="2">Latest Issues</Heading>
+      <Heading size="6" mb="2">
+        Latest Issues
+      </Heading>
       <Table.Root>
         <Table.Body>
           {issues.map((issue) => (
